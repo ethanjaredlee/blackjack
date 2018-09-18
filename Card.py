9ba93215ce2c4c18e41ba84f3eb37d91ef567904
Ethan Lee <ethanlee2020@u.northwestern.edu>
2018-09-18 15:20:15 +0900

    initialized card

---
+++ Card.py
@@ -0,0 +1,28 @@
+class Card(object):
+    """ 
+    Class representing a single card
+    """
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+  
+    def __repr__(self):
+        return "[" + self.rank + "|" + self.suit + "]"
+
+    def getValue(self, player):
+        if self.rank in range(2, 11):
+            # number card
+            return self.rank
+        elif self.rank in ["J", "Q", "K"]:
+            # face card
+            return 10
+        elif self.rank == "A":
+            # ace card
+            if player.score > 10:
+                return 1
+            else:
+                return 11
+        else:
+            # none of the above
+            raise ValueError("Card rank is not a standard 2-10, J, Q, K, or A value")
+        
