d97e8ae4026bdecb5e3119c859161b6b99934898
Ethan Lee <ethanlee2020@u.northwestern.edu>
2018-09-18 16:18:50 +0900

    finished card class

--- Game/Card.py
+++ Game/Card.py
@@ -3,32 +3,14 @@ import constants
 class Card(object):
     """ 
     Class representing a single card
+    Note: Cards do not have intrinsic values, hands do
     """
     def __init__(self, rank, suit):
-        # check rank is value
-        assert rank in constants.CARD_RANKS
-        assert suit in constants.CARD_SUITS
+        assert rank in constants.CARD_RANKS, "Rank does not exist"
+        assert suit in constants.CARD_SUITS, "Suit does not exist"
 
         self.rank = rank
         self.suit = suit
   
     def __repr__(self):
         return "[" + self.rank + "|" + self.suit + "]"
-
-    def getValue(self, player):
-        if self.rank in [str(n) for n in range(2,11)]:
-            # number card
-            return self.rank
-        elif self.rank in ["J", "Q", "K"]:
-            # face card
-            return 10
-        elif self.rank == "A":
-            # ace card
-            if player.score > 10:
-                return 1
-            else:
-                return 11
-        else:
-            # none of the above
-            raise ValueError("Card rank is not a standard 2-10, J, Q, K, or A value")
-        
--- Tests/test_card.py
+++ Tests/test_card.py
@@ -1,10 +1,22 @@
-from Game.Card import Card
+import Game.Card as Card
+import Game.constants as constants
 import unittest
 
 class CardTestCase(unittest.TestCase):
     def test_card_construction(self):
-        card = Card("J", "Spade")
-        self.assertEqual(card.rank, "J")
+        for rank in constants.CARD_RANKS:
+            for suit in constants.CARD_SUITS:
+                card = Card.Card(rank, suit)
+                self.assertEqual(card.rank, rank)
+                self.assertEqual(card.suit, suit)
+
+    def test_bad_rank(self):
+        with self.assertRaises(AssertionError):
+            Card.Card(1, "Spade")
+
+    def test_bad_suit(self):
+        with self.assertRaises(AssertionError):
+            Card.Card(2, "Joker")
 
 if __name__ == "__main__":
     unittest.main()
