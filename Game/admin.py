a911cc91c5ed196f80b2d8a52379573425ccf159
Ethan Lee <ethanlee2020@u.northwestern.edu>
2018-09-18 16:56:52 +0900

    shuffle deck

--- Game/admin.py
+++ Game/admin.py
@@ -1,11 +1,18 @@
 from Card import Card
 import constants
+import random
 
 class Admin(object):
 
     def __init__(self, decks):
         assert decks > 0, "Number of decks must be greater than 0"
 
+        self.deckNumber = decks
         self.pile = []
         for _ in range(decks):
             self.pile.extend([Card(rank, suit) for rank in constants.CARD_RANKS for suit in constants.CARD_SUITS])
+
+    def shuffle(self):
+        assert len(self.pile) == self.deckNumber * 52, "Full deck is needed to shuffle"
+
+        random.shuffle(self.pile)
